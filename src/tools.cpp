917dc5e9e2b16e7ba7e5cd4a3ddd3d3f6001cca0
Rudy Pena <penar@us.ibm.com>
2017-05-19 16:17:36 -0500

    adding rmse logic into tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,11 +15,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-}
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+    
+    if (estimations.size() != ground_truth.size() || estimations.size() == 0){
+        cout << "Invalid estimation or ground truth data" << endl;
+        return rmse;
+    }
+    
+    for (unsigned int i=0; i < estimations.size(); i++) {
+        VectorXd residual = estimations[i] - ground_truth[i];
+        
+        // Coefficient-wise Multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+    
+    // Calculate the mean
+    rmse = rmse/estimations.size();
+    
+    // Calculate the squared root
+    rmse = rmse.array().sqrt();
+    
+    return rmse;
+};
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
   TODO:
     * Calculate a Jacobian here.
   */
-}
+};
