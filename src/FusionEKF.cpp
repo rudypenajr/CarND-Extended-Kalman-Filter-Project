ed617e6ec597e1d59f711dcdfd5dec65c5d99e1a
Rudy Pena <penar@us.ibm.com>
2017-05-21 14:23:33 -0500

    fixing initialization logic btwn radar and lidar -- adding Q logic for prediction

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -60,22 +60,38 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     */
     // first measurement
     cout << "EKF: " << endl;
-    ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    // ekf_.x_ = VectorXd(4);
+    // ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      // Populate position and velocity
+      ekf_.x_ << PolarToCartesian(measurement_pack.raw_measurements_);
+      // Confident in both position and velocity
+      ekf_.P_ << 1, 0, 0, 0,
+                  0, 1, 0, 0,
+                  0, 0, 1, 0,
+                  0, 0, 0, 1;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      // Populate position
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+      ekf_.x_(1) = measurement_pack.raw_measurements_(1);
+      // Unsure about velocity
+      ekf_.P_ << 1, 0, 0, 0,
+                  0, 1, 0, 0,
+                  0, 0, 1000, 0,
+                  0, 0, 0, 1000;
     }
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
+    previous_timestamp_ = measurement_pack.timestamp_;
     return;
   }
 
@@ -90,6 +106,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+
+   // compute the time elapsed between the current and previous measurements (Lesson 13)
+   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+   previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  //Modify the F matrix so that the time is integrated
+  cout << "print dt ===> " << dt << endl;
+  kf_.F_(0, 2) = dt;
+  kf_.F_(1, 3) = dt;
+
+  kf_.Q_ = MatrixXd(4, 4);
+  kf_.Q_ <<  dt_4/4*ekf_.noise_ax, 0, dt_3/2*ekf_.noise_ax, 0,
+              0, dt_4/4*ekf_.noise_ay, 0, dt_3/2*ekf_.noise_ay,
+              dt_3/2*ekf_.noise_ax, 0, dt_2*ekf_.noise_ax, 0,
+              0, dt_3/2*ekf_.noise_ay, 0, dt_2*ekf_.noise_ay;
 
   ekf_.Predict();
 
